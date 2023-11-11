88f60e91545c407494a6cf3ce87012129e067d58
LuongXuanThang20110724 <20110724@student.hcmute.edu.vn>
2023-11-11 23:10:46 +0700

    fix swagger to deploy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { createDocument } from './swagger/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
+
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +16,21 @@ async function bootstrap() {
       persistAuthorization: true,
     }
   });
+
+
+   // get the swagger json file (if app is running in development mode)
+   if (process.env.NODE_ENV === 'development') {
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger');
+
+    // write swagger json file
+    const pathToSwaggerJson = resolve(
+      pathToSwaggerStaticFolder,
+      'swagger.ts',
+    );
+    const swaggerJson = JSON.stringify(document, null, 2);
+    writeFileSync(pathToSwaggerJson, swaggerJson);
+    console.log(`Swagger JSON file written to: '/swagger/swagger.ts'`);
+  }
 
   app.useGlobalPipes(new ValidationPipe());
 
