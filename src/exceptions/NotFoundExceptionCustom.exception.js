4673e9f926f036fde9e86a57a927069be4a618e7
LuongXuanThang20110724 <20110724@student.hcmute.edu.vn>
2023-11-12 00:43:34 +0700

    fix 11

--- src/exceptions/NotFoundExceptionCustom.exception.js
+++ src/exceptions/NotFoundExceptionCustom.exception.js
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus } from "@nestjs/common";
 import { Translate } from "./Translate";
 
 export class NotFoundExceptionCustom extends HttpException {
-  constructor(name: string) {
+  constructor(name) {
     super(`Không tìm thấy ${Translate[name]}`, HttpStatus.NOT_FOUND);
   }
 }
